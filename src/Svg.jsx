2b08574d07baf801688eff9f61d720db4943090d
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-06-20 23:28:55 +0530

    svg aimation string flicker

--- src/Svg.jsx
+++ src/Svg.jsx
@@ -1,30 +1,31 @@
 import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 const Svg = () => {
   const stringRef = useRef(null);
 
-  useEffect(() => {
-    const finalPath = "M 10 100 Q 400 100 790 100";
-    const string = stringRef.current;
+  useGSAP(() => {
+    var finalPath = "M 10 100 Q 400 100 790 100";
+    var string = stringRef.current;
 
     const handleMouseMove = (event) => {
       const rect = string.getBoundingClientRect();
       const y = event.clientY - rect.top; // Calculate y relative to the element
 
-      const path = `M 10 100 Q 400 ${y} 790 100`;
-      gsap.to("svg path", {
+      var path = `M 10 100 Q ${event.clientX} ${y} 790 100`;
+      gsap.to("svg .path", {
         attr: { d: path },
         duration: 0.3,
         ease: "power3.out"
       });
     };
 
-    const handleMouseLeave = () => {
-      gsap.to("svg path", {
-        attr: { d: finalPath },
+    function handleMouseLeave  () {
+      gsap.to("svg .path", {
+        attr: { d: "M 10 100 Q 400 100 790 100" },
         duration: 1.5,
-        ease: "elastic.out(1, 0.2)"
+        ease: "elastic.out(1,0.3)"
       });
     };
 
@@ -42,7 +43,7 @@ const Svg = () => {
   }, []);
 
   return (
-    <div className="string w-[100%] h-[200px]" ref={stringRef}>
+    <div className="string-container w-[100%] h-[200px]" ref={stringRef}>
       <svg width="800" height="200">
         <path className="path" d="M 10 100 Q 400 100 790 100" stroke="white" fill="transparent" />
       </svg>
