d7a8f6597b8321cdc3ec4dcfb37ef6949099a42c
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-06-21 21:27:45 +0530

    cursor animation created

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import './App.css'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { useRef } from 'react';
+
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Svg from './Svg';
 gsap.registerPlugin(ScrollTrigger);
 function App() {
   const [count, setCount] = useState(0);
+  const main=useRef(null);
+  const cursor=useRef(null);
+  const imagediv=useRef(null);
   var t1=gsap.timeline();
   useGSAP(()=>{
     // t1.to('.box',
@@ -135,69 +139,125 @@ gsap.from(".page3 .box",{
 //   }
 // })
 
+var image=imagediv.current;
+var cursorref=cursor.current;
+
+
+main.current.addEventListener("mousemove",function(dets){
+    gsap.to(".cursor",{
+      x:dets.x,
+      y:dets.y,
+      duration:0.5,
+      ease:"back.out"
+    })
+})
+
+image.addEventListener("mousemove",function(){
+  
+ cursorref.innerHTML="view  more"
+  gsap.to(cursorref,{
+    scale:2,
+    duration:0.5,
+    backgroundColor:"#ffffff8a"
+    
+  
+  })
+
+  
+})
+
+
+image.addEventListener("mouseleave",function(){
+ cursorref.innerHTML=""
+  gsap.to(cursorref,{
+    scale:1,
+    duration:0.5,
+    backgroundColor:"#fff"
+  })
+})
+
+
 
 
   })
   return (
-    <div className='bg-black h-[100vh] '>
+   <>
+   <div ref={cursor} className=' z-10 cursor  flex items-center text-center justify-center text-[6px]  h-[20px] w-[20px] fixed  rounded-full bg-white'></div>
+   <div ref={main} className='main flex items-center justify-center bg-black h-[100vh] '>
+    <div  ref={imagediv}  className='Image relative h-[30vh] w-[60vw] '>
     
+    <img
+   
+
+    src='https://images.unsplash.com/photo-1517405030045-45f7ad942106?q=80&w=1928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    className=' h-[100%] w-[100%] absolute object-cover'
+    />
+    <div className='overlay bg-transparent z-20 w-[100%] h-[100%] absolute'></div>
+    </div>
+{/**
+ <div className="box rounded-full w-[300px] h-[300px] bg-red-300"></div>
+<div className="box2 rounded-full w-[300px] h-[300px] bg-red-300"></div>
+<div className="box3 rounded-full w-[300px] h-[300px] bg-red-300"></div>
+ 
+ <h1 className='text-white '>Hello I am Sanjeev</h1>
+ <h1 className='text-white'>Hello </h1>
+ <h1 className='text-white'>Hello Buddy</h1>
+ */}
+
  {/**
-  <div className="box rounded-full w-[300px] h-[300px] bg-red-300"></div>
- <div className="box2 rounded-full w-[300px] h-[300px] bg-red-300"></div>
- <div className="box3 rounded-full w-[300px] h-[300px] bg-red-300"></div>
-  
-  <h1 className='text-white '>Hello I am Sanjeev</h1>
-  <h1 className='text-white'>Hello </h1>
-  <h1 className='text-white'>Hello Buddy</h1>
-  */}
+   Navbar
+    <div className="flex pt-4 text-white items-center justify-between">
+   <h1>DevGods</h1>
+   <div className="flex gap-2 items-center justify-between">
+     <h4>Home</h4>
+     <h4>About</h4>
+     <h4>Projects</h4>
+     <h4>Contact</h4>
+   </div>
+ </div>
+ <h2 className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-5xl">
+ Sanjeev Portfolio
+</h2>
+   */}
 
   {/**
-    Navbar
-     <div className="flex pt-4 text-white items-center justify-between">
-    <h1>DevGods</h1>
-    <div className="flex gap-2 items-center justify-between">
-      <h4>Home</h4>
-      <h4>About</h4>
-      <h4>Projects</h4>
-      <h4>Contact</h4>
-    </div>
-  </div>
-  <h2 className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-5xl">
-  Sanjeev Portfolio
-</h2>
-    */}
+   Scroll trigger
+    <div className="page1 h-[100%] flex items-center justify-center w-[100%] bg-blue-300">
+     <div className="box h-[300px] bg-red-400 w-[300px]"></div>
+   </div>
+   <div className="page2 h-[100%]  flex items-center justify-center  w-[100%] bg-blue-500">
+   <div className="box h-[300px] bg-red-400 w-[300px]"></div>
+   </div>
+   <div className="page3 h-[100%]   flex items-center justify-center w-[100%] bg-blue-800">
+   <div className="box h-[300px] bg-red-400 w-[300px]"></div>
+   </div>
+   */}
 
    {/**
-    Scroll trigger
-     <div className="page1 h-[100%] flex items-center justify-center w-[100%] bg-blue-300">
-      <div className="box h-[300px] bg-red-400 w-[300px]"></div>
-    </div>
-    <div className="page2 h-[100%]  flex items-center justify-center  w-[100%] bg-blue-500">
-    <div className="box h-[300px] bg-red-400 w-[300px]"></div>
-    </div>
-    <div className="page3 h-[100%]   flex items-center justify-center w-[100%] bg-blue-800">
-    <div className="box h-[300px] bg-red-400 w-[300px]"></div>
-    </div>
-    */}
- 
-    {/**
-       <div className="page1 h-[100%] w-[100%] bg-black">
-      <div className="box">
-      </div>
-    </div>
-    <div className="page2 overflow-x-hidden h-[100%] w-[100%] bg-blue-200">
-    <div className="box text-[30vw] font-bold uppercase">
-    Experineces
-    </div>
-  </div>
-  <div className="page3 h-[100%] w-[100%] bg-black">
-  <div className="box">
-  </div>
+      <div className="page1 h-[100%] w-[100%] bg-black">
+     <div className="box">
+     </div>
+   </div>
+   <div className="page2 overflow-x-hidden h-[100%] w-[100%] bg-blue-200">
+   <div className="box text-[30vw] font-bold uppercase">
+   Experineces
+   </div>
+ </div>
+ <div className="page3 h-[100%] w-[100%] bg-black">
+ <div className="box">
+ </div>
 </div>
-      */}
-   <Svg/>
+     */}
+ {
+  /**
+   * 
+   *  <Svg/>
+   */
+ }
  
-    </div>
+
+   </div>
+   </>
     
   )
 }
